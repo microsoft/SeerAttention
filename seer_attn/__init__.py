ae0cf7161cc9a1083bc7c76124d549602111aa79
YizhaoGao <yizhao@connect.hku.hk>
2024-11-11 11:01:34 +0000

    extract common module in model definition

--- seer_attn/__init__.py
+++ seer_attn/__init__.py
@@ -1,7 +1,9 @@
 from seer_attn.modeling_llama_seerattn_pt import LlamaForCausalLMSeerAttnPT
 from seer_attn.modeling_llama_seerattn_inference import LlamaForCausalLMSeerAttnInf
+from seer_attn.modeling_llama_seerattn_ft import LlamaForCausalLMSeerAttnFT
 
 __all__ = [
     "LlamaForCausalLMSeerAttnPT"
     "LlamaForCausalLMSeerAttnInf"
+    "LlamaForCausalLMSeerAttnFT"
 ]
--- seer_attn/modeling_llama_seerattn_inference.py
+++ seer_attn/modeling_llama_seerattn_inference.py
@@ -37,220 +37,12 @@ from seer_attn.attn_gate import ATTNGATE_CLASSES
 logger = logging.get_logger(__name__)
 
 _CONFIG_FOR_DOC = "LlamaConfig"
-from .utils import BaseModelOutputWithPastAndMask, CausalLMOutputWithPastAndMask
+from seer_attn.utils import BaseModelOutputWithPastAndMask, CausalLMOutputWithPastAndMask
+import copy
 
-class LlamaRMSNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-6):
-        """
-        LlamaRMSNorm is equivalent to T5LayerNorm
-        """
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = eps
-
-    def forward(self, hidden_states):
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
-
+from seer_attn.common_module import *
 
 ALL_LAYERNORM_LAYERS.append(LlamaRMSNorm)
-
-
-
-class LlamaRotaryEmbedding(nn.Module):
-    def __init__(
-        self,
-        dim=None,
-        max_position_embeddings=2048,
-        base=10000,
-        device=None,
-        scaling_factor=1.0,
-        rope_type="default",
-        config: Optional[LlamaConfig] = None,
-    ):
-        super().__init__()
-        # TODO (joao): remove the `if` below, only used for BC
-        self.rope_kwargs = {}
-        if config is None:
-            logger.warning_once(
-                "`LlamaRotaryEmbedding` can now be fully parameterized by passing the model config through the "
-                "`config` argument. All other arguments will be removed in v4.45"
-            )
-            self.rope_kwargs = {
-                "rope_type": rope_type,
-                "factor": scaling_factor,
-                "dim": dim,
-                "base": base,
-                "max_position_embeddings": max_position_embeddings,
-            }
-            self.rope_type = rope_type
-            self.max_seq_len_cached = max_position_embeddings
-            self.original_max_seq_len = max_position_embeddings
-        else:
-            # BC: "rope_type" was originally "type"
-            if config.rope_scaling is not None:
-                self.rope_type = config.rope_scaling.get("rope_type", config.rope_scaling.get("type"))
-            else:
-                self.rope_type = "default"
-            self.max_seq_len_cached = config.max_position_embeddings
-            self.original_max_seq_len = config.max_position_embeddings
-
-        self.config = config
-        self.rope_init_fn = ROPE_INIT_FUNCTIONS[self.rope_type]
-
-        inv_freq, self.attention_scaling = self.rope_init_fn(self.config, device, **self.rope_kwargs)
-        self.register_buffer("inv_freq", inv_freq, persistent=False)
-        self.original_inv_freq = self.inv_freq
-
-    def _dynamic_frequency_update(self, position_ids, device):
-        """
-        dynamic RoPE layers should recompute `inv_freq` in the following situations:
-        1 - growing beyond the cached sequence length (allow scaling)
-        2 - the current sequence length is in the original scale (avoid losing precision with small sequences)
-        """
-        seq_len = torch.max(position_ids) + 1
-        if seq_len > self.max_seq_len_cached:  # growth
-            inv_freq, self.attention_scaling = self.rope_init_fn(
-                self.config, device, seq_len=seq_len, **self.rope_kwargs
-            )
-            self.register_buffer("inv_freq", inv_freq, persistent=False)  # TODO joao: may break with compilation
-            self.max_seq_len_cached = seq_len
-
-        if seq_len < self.original_max_seq_len and self.max_seq_len_cached > self.original_max_seq_len:  # reset
-            self.register_buffer("inv_freq", self.original_inv_freq, persistent=False)
-            self.max_seq_len_cached = self.original_max_seq_len
-
-    @torch.no_grad()
-    def forward(self, x, position_ids):
-        if "dynamic" in self.rope_type:
-            self._dynamic_frequency_update(position_ids, device=x.device)
-
-        # Core RoPE block
-        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
-        position_ids_expanded = position_ids[:, None, :].float()
-        # Force float32 (see https://github.com/huggingface/transformers/pull/29285)
-        device_type = x.device.type
-        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
-        with torch.autocast(device_type=device_type, enabled=False):
-            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
-            emb = torch.cat((freqs, freqs), dim=-1)
-            cos = emb.cos()
-            sin = emb.sin()
-
-        # Advanced RoPE types (e.g. yarn) apply a post-processing scaling factor, equivalent to scaling attention
-        cos = cos * self.attention_scaling
-        sin = sin * self.attention_scaling
-
-        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
-
-
-class LlamaLinearScalingRotaryEmbedding(LlamaRotaryEmbedding):
-    """LlamaRotaryEmbedding extended with linear scaling. Credits to the Reddit user /u/kaiokendev"""
-
-    def __init__(self, *args, **kwargs):
-        logger.warning_once(
-            "`LlamaLinearScalingRotaryEmbedding` is deprecated an will be removed in v4.45. Please use "
-            "`LlamaRotaryEmbedding`, which now also does linear scaling (simply pass the model config to __init__)."
-        )
-        kwargs["rope_type"] = "linear"
-        super().__init__(*args, **kwargs)
-
-
-class LlamaDynamicNTKScalingRotaryEmbedding(LlamaRotaryEmbedding):
-    """LlamaRotaryEmbedding extended with Dynamic NTK scaling. Credits to the Reddit users /u/bloc97 and /u/emozilla"""
-
-    def __init__(self, *args, **kwargs):
-        logger.warning_once(
-            "`LlamaDynamicNTKScalingRotaryEmbedding` is deprecated an will be removed in v4.45. Please use "
-            "`LlamaRotaryEmbedding`, which now also does dynamic ntk scaling (simply pass the model config to "
-            "__init__)."
-        )
-        kwargs["rope_type"] = "dynamic"
-        super().__init__(*args, **kwargs)
-
-
-def rotate_half(x):
-    """Rotates half the hidden dims of the input."""
-    x1 = x[..., : x.shape[-1] // 2]
-    x2 = x[..., x.shape[-1] // 2 :]
-    return torch.cat((-x2, x1), dim=-1)
-
-
-def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
-    """Applies Rotary Position Embedding to the query and key tensors.
-
-    Args:
-        q (`torch.Tensor`): The query tensor.
-        k (`torch.Tensor`): The key tensor.
-        cos (`torch.Tensor`): The cosine part of the rotary embedding.
-        sin (`torch.Tensor`): The sine part of the rotary embedding.
-        position_ids (`torch.Tensor`, *optional*):
-            Deprecated and unused.
-        unsqueeze_dim (`int`, *optional*, defaults to 1):
-            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
-            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
-            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
-            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
-            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
-            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
-    Returns:
-        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
-    """
-    cos = cos.unsqueeze(unsqueeze_dim)
-    sin = sin.unsqueeze(unsqueeze_dim)
-    q_embed = (q * cos) + (rotate_half(q) * sin)
-    k_embed = (k * cos) + (rotate_half(k) * sin)
-    return q_embed, k_embed
-
-
-class LlamaMLP(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
-        self.hidden_size = config.hidden_size
-        self.intermediate_size = config.intermediate_size
-        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.mlp_bias)
-        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.mlp_bias)
-        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=config.mlp_bias)
-        self.act_fn = ACT2FN[config.hidden_act]
-
-    def forward(self, x):
-        if self.config.pretraining_tp > 1:
-            slice = self.intermediate_size // self.config.pretraining_tp
-            gate_proj_slices = self.gate_proj.weight.split(slice, dim=0)
-            up_proj_slices = self.up_proj.weight.split(slice, dim=0)
-            down_proj_slices = self.down_proj.weight.split(slice, dim=1)
-
-            gate_proj = torch.cat(
-                [F.linear(x, gate_proj_slices[i]) for i in range(self.config.pretraining_tp)], dim=-1
-            )
-            up_proj = torch.cat([F.linear(x, up_proj_slices[i]) for i in range(self.config.pretraining_tp)], dim=-1)
-
-            intermediate_states = (self.act_fn(gate_proj) * up_proj).split(slice, dim=2)
-            down_proj = [
-                F.linear(intermediate_states[i], down_proj_slices[i]) for i in range(self.config.pretraining_tp)
-            ]
-            down_proj = sum(down_proj)
-        else:
-            down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
-
-        return down_proj
-
-
-def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
-    """
-    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
-    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
-    """
-    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
-    if n_rep == 1:
-        return hidden_states
-    hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
-    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
-
 
 class LlamaSeerAttention(nn.Module):
     """Attention module with seer attention post training setting"""
@@ -289,7 +81,7 @@ class LlamaSeerAttention(nn.Module):
         self.attn_gate = ATTNGATE_CLASSES[config.attn_gate_type](
             config.gate_block_size, 
             self.head_dim, 
-            self.head_dim, 
+            config.gate_hidden_size, 
             num_k_head=self.num_key_value_heads, 
             num_q_head=self.num_heads
         )
@@ -309,8 +101,8 @@ class LlamaSeerAttention(nn.Module):
         use_cache: bool = False,
         cache_position: Optional[torch.LongTensor] = None,
         position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.45
-        position_embeddings_ds: Optional[Tuple[torch.Tensor, torch.Tensor]] = None, ## downsampled position embeddings
-        attention_mask_ds: Optional[torch.Tensor] = None, ## downsampled attention mask
+        block_position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None, ## downsampled position embeddings
+        block_attention_mask: Optional[torch.Tensor] = None, ## downsampled attention mask
         **kwargs,
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
 
@@ -350,7 +142,7 @@ class LlamaSeerAttention(nn.Module):
         if self.config.nz_ratio < 1 and q_len > 1:
             topk_nz_ratio = 1 - math.sqrt(1 - self.config.nz_ratio)
             downsampled_len = math.ceil(key_states.shape[-2] / self.config.gate_block_size)
-            predict_mask = self.attn_gate(q_, k_, attention_mask_ds, position_embeddings_ds, is_training=False)
+            predict_mask = self.attn_gate(q_, k_, block_attention_mask, block_position_embeddings, is_training=False)
 
             topk = int(topk_nz_ratio * downsampled_len)
             if topk == 0:
@@ -415,8 +207,8 @@ class LlamaDecoderLayer(nn.Module):
         use_cache: Optional[bool] = False,
         cache_position: Optional[torch.LongTensor] = None,
         position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.45
-        position_embeddings_ds: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
-        attention_mask_ds: Optional[torch.Tensor] = None,
+        block_position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+        block_attention_mask: Optional[torch.Tensor] = None,
         **kwargs,
     ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
         """
@@ -440,8 +232,8 @@ class LlamaDecoderLayer(nn.Module):
             kwargs (`dict`, *optional*):
                 Arbitrary kwargs to be ignored, used for FSDP and other methods that injects code
                 into the model
-            position_embeddings_ds (`Tuple[torch.FloatTensor, torch.FloatTensor]`, *optional*): downsampled positional embeddings
-            attention_mask_ds (`torch.FloatTensor`, *optional*): downsampled attention mask: downsampled attention mask
+            block_position_embeddings (`Tuple[torch.FloatTensor, torch.FloatTensor]`, *optional*): downsampled positional embeddings
+            block_attention_mask (`torch.FloatTensor`, *optional*): downsampled attention mask: downsampled attention mask
         """
         residual = hidden_states
 
@@ -459,8 +251,8 @@ class LlamaDecoderLayer(nn.Module):
             use_cache=use_cache,
             cache_position=cache_position,
             position_embeddings=position_embeddings,
-            position_embeddings_ds=position_embeddings_ds,
-            attention_mask_ds=attention_mask_ds,
+            block_position_embeddings=block_position_embeddings,
+            block_attention_mask=block_attention_mask,
             **kwargs,
         )
         hidden_states = residual + hidden_states
@@ -627,6 +419,9 @@ class LlamaModel(LlamaPreTrainedModel):
         )
         self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.rotary_emb = LlamaRotaryEmbedding(config=config)
+        block_config = copy.deepcopy(config)
+        block_config.hidden_size = config.gate_hidden_size * config.num_attention_heads
+        self.block_rotary_emb = LlamaRotaryEmbedding(config=block_config)
         self.gradient_checkpointing = False
 
         # Initialize weights and apply final processing
@@ -692,15 +487,15 @@ class LlamaModel(LlamaPreTrainedModel):
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
 
-        causal_mask, casual_mask_ds = self._update_causal_mask(
+        causal_mask, block_causal_mask = self._update_causal_mask(
             attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
         )
         hidden_states = inputs_embeds
 
         # create position embeddings to be shared across the decoder layers
         position_embeddings = self.rotary_emb(hidden_states, position_ids)
-        position_ids_ds = position_ids[:, 0::self.config.gate_block_size] ## downsampled position ids
-        position_embeddings_ds = self.rotary_emb(hidden_states, position_ids_ds)
+        block_position_ids = position_ids[:, 0::self.config.gate_block_size] ## downsampled position ids
+        block_position_embeddings = self.block_rotary_emb(hidden_states, block_position_ids)
 
 
         # print("position_embeddings", position_embeddings[0].shape, position_embeddings[1].shape)
@@ -730,8 +525,8 @@ class LlamaModel(LlamaPreTrainedModel):
                     use_cache,
                     cache_position,
                     position_embeddings,
-                    position_embeddings_ds,
-                    casual_mask_ds,
+                    block_position_embeddings,
+                    block_causal_mask,
                 )
             else:
                 layer_outputs = decoder_layer(
@@ -743,8 +538,8 @@ class LlamaModel(LlamaPreTrainedModel):
                     use_cache=use_cache,
                     cache_position=cache_position,
                     position_embeddings=position_embeddings,
-                    position_embeddings_ds=position_embeddings_ds,
-                    attention_mask_ds=casual_mask_ds,
+                    block_position_embeddings=block_position_embeddings,
+                    block_attention_mask=block_causal_mask,
                 )
 
             hidden_states = layer_outputs[0]
