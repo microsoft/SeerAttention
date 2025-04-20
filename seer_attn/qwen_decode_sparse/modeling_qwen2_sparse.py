fix sparsity calculation

seer_attn/qwen_decode_sparse/modeling_qwen2_sparse.py
@@ -722,7 +722,7 @@ class SeerDecodingQwen2ForCausalLM(Qwen2PreTrainedModel, GenerationMixin):
             if active_local.sum().item() < cur_to_orig.shape[0]:
                 active_indices_local = torch.nonzero(active_local, as_tuple=False).squeeze(-1)
                 # Update the kv cache using indices relative to the current cache.
-                # print("active_indices_local", active_indices_local, "kvlen:", attention_mask.shape[1], flush=True)
+                print("active_indices_local", active_indices_local, "kvlen:", attention_mask.shape[1], flush=True)
                 current_kvcache.batch_select_indices(active_indices_local)
                 if not self.config.seerattn_use_oracle_sparse:
                     current_kcompressed_cache.batch_select_indices(active_indices_local)