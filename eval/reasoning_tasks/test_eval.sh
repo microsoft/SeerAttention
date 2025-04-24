4c496f39820ab8d1397b34e4056626ee6dd6d974
xiboliyaxiangjiaojun <1937671237@qq.com>
2025-04-24 02:58:13 +0000

    finish running

--- eval/reasoning_tasks/test_eval.sh
+++ eval/reasoning_tasks/test_eval.sh
@@ -9,21 +9,23 @@ CUDA_VISIBLE_DEVICES=0 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python e
     --batch_size $bs \
     --limit -1 \
     --output_dir ./results_sparse_aime \
-    --attention_implementation ours \
+    --attention_implementation seer_sparse \
     --threshold $threshold \
     --use_batch_exist \
     --use_fused_kernel \
+    --profile_sparsity \
     --surround_with_messages 
 
 
-# ## Dense Baseline
-# model_dir="deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
-# CUDA_VISIBLE_DEVICES=0 python eval.py \
-#     --model_name_or_path $model_dir \
-#     --data_name $task \
-#     --batch_size $bs \
-#     --limit -1 \
-#     --output_dir ./results_sparse_aime \
-#     --attention_implementation fa2 \
-#     --use_batch_exist \
-#     --surround_with_messages 
+
+## Dense Baseline
+model_dir="deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    --model_name_or_path $model_dir \
+    --data_name $task \
+    --batch_size $bs \
+    --limit -1 \
+    --output_dir ./results_sparse_aime \
+    --attention_implementation fa2 \
+    --use_batch_exist \
+    --surround_with_messages 
