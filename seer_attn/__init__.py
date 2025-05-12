8c99db55557bdcac2733250bdce3b0fb5e637f75
Shijie Cao <caoshijie0501@gmail.com>
2025-05-12 15:04:12 +0800

    add qwen3 (#21)

--- seer_attn/__init__.py
+++ seer_attn/__init__.py
@@ -1,11 +1,13 @@
 
 from seer_attn.llama.modeling_llama_seerattn import SeerAttnLlamaForCausalLM
 from seer_attn.qwen.modeling_qwen2_seerattn import SeerAttnQwen2ForCausalLM
-from seer_attn.decode_sparse.qwen2.modeling_qwen2 import SeerDecodingQwen2ForCausalLM
-from seer_attn.decode_sparse.phi3.modeling_phi3 import SeerDecodingPhi3ForCausalLM
+from seer_attn.decode_sparse.qwen2.modeling_qwen2_seerattn import SeerDecodingQwen2ForCausalLM
+from seer_attn.decode_sparse.qwen3.modeling_qwen3_seerattn import SeerDecodingQwen3ForCausalLM
+from seer_attn.decode_sparse.phi3.modeling_phi3_seerattn import SeerDecodingPhi3ForCausalLM
 __all__ = [
     "SeerAttnLlamaForCausalLM",
     "SeerAttnQwen2ForCausalLM",
     "SeerDecodingQwen2ForCausalLM",
+    "SeerDecodingQwen3ForCausalLM",
     "SeerDecodingPhi3ForCausalLM",
 ]
